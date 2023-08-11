56594ad67bc76fbdca4e3dd037cca7df9e6f9a23
Bruno Brasolin <brunobrasolinc@gmail.com>
2023-08-10 21:09:11 -0300

    Adicionar arquivos de projeto.

---
+++ Models/RabbitMQ/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamidas.Utils.Models.RabbitMQ;
+
+record EmailModel(string Recipient, string Body, string Subject);
